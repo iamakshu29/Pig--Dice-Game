0a5607c25ad33eea6f14bddcd8388644984382b8
Akshat Verma <iiamakshu@gmail.com>
2021-05-06 22:55:07 +0530

    First

---
+++ projJS.js
@@ -0,0 +1,99 @@
+let score,roundScore,activePlayer,gamePlaying;
+init();
+
+// Below Function is used for the roll-dice button
+
+document.querySelector(".btn--roll").addEventListener('click', function()
+{
+    if(gamePlaying)
+    {
+        let dice = Math.floor(Math.random()*6+1);
+    
+        document.querySelector(".dice").style.display = 'block';
+        document.querySelector(".dice").src = 'dice-' + dice + '.png';
+
+        if(dice !== 1)
+        {
+            roundScore += dice;
+            document.querySelector("#current--" + activePlayer).textContent = roundScore;  
+        }
+        else
+        {   
+            nextPlayer();
+        }
+    }
+});  
+
+// Below Function is used for the hold button
+
+    document.querySelector(".btn--hold").addEventListener('click', function()
+    {
+        if(gamePlaying)
+        {
+            score[activePlayer] += roundScore;
+            document.querySelector("#score--" + activePlayer).textContent = score[activePlayer];
+            
+            // What do you want the winning score
+            if(score[activePlayer] >= 30)
+            {
+                document.querySelector("#name--" + activePlayer).textContent = "Winner";
+                document.querySelector(".dice").style.display = 'none';
+                document.querySelector(".player--" + activePlayer).classList.add('winner');
+                document.querySelector(".player--" + activePlayer).classList.remove('player--active');
+                gamePlaying = false; 
+            }
+            else
+            {
+                nextPlayer();
+            } 
+        }
+         
+    });
+
+// // Below Function is used for switching the player     
+function nextPlayer()
+{
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById("current--0").textContent = roundScore;
+    document.getElementById("current--1").textContent = roundScore;
+
+    document.querySelector(".player--0").classList.toggle('player--active');
+    document.querySelector(".player--1").classList.toggle('player--active');
+
+    document.querySelector(".dice").style.display = 'none';
+}
+
+
+// init function is invoked when the new game button is being pressed..
+
+document.querySelector(".btn--new").addEventListener('click',init);
+
+
+
+// Below Function is used for the new game as to initialize all the things to 0
+
+function init()
+{
+    score = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector(".dice").style.display = 'none';
+
+    document.querySelector("#current--0").textContent = '0';
+    document.querySelector("#current--1").textContent = '0';
+    document.querySelector("#score--0").textContent = '0';
+    document.querySelector("#score--1").textContent = '0';
+
+    document.querySelector("#name--0").textContent = "PLAYER 1";
+    document.querySelector("#name--1").textContent = "PLAYER 2";
+
+    document.querySelector(".player--0").classList.remove('winner');
+    document.querySelector(".player--1").classList.remove('winner');
+    document.querySelector(".player--0").classList.remove('player--active');
+    document.querySelector(".player--0").classList.remove('player--active');
+    document.querySelector(".player--1").classList.add('player--active');
+}
